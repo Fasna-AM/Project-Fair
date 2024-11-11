9c1ba5e2a214ebdde57aabba44972e9f8c9c1de3
unknown <Fasna-AM>
2024-11-11 11:55:32 +0530

    changes2

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,37 @@ import Auth from './pages/Auth'
 import Projects from './pages/Projects'
 import Dashboard from './pages/Dashboard'
 import Footer from './components/Footer'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { tokenAuthContext } from './context/AuthContextAPI'
+import Pnf from './pages/Pnf'
 
 function App() {
  const {isAutherised,setIsAutherised} = useContext(tokenAuthContext)
+ useEffect(()=>{
+  if(sessionStorage.getItem("token")){
+    setIsAutherised(true)
+  }else{
+    setIsAutherised(false)
+  }
+ },[isAutherised])
+ console.log(isAutherised);
+ 
   return (
     <>
     <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/dashboard' element={isAutherised? <Dashboard/> : <Navigate to={'/login'}/>}/>
-      <Route path='/projects' element={isAutherised? <Projects/> : <Navigate to={'/login'}/>}/>
+      {
+        isAutherised &&
+        <>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+        </>
+      }
+      {/* <Route path='/dashboard' element={isAutherised? <Dashboard/> : <Navigate to={'/login'}/>}/> */}
+      {/* <Route path='/projects' element={isAutherised? <Projects/> : <Navigate to={'/login'}/>}/> */}
       <Route path='/login' element={<Auth/>}/>
       <Route path='/register' element={<Auth insideRegister={true} />}/>
+      <Route path='/*' element={<Pnf/>}/>
     </Routes>
     <Footer/>
     </>
